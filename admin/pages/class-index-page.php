656d5f24354126424579b946fefe4e0f6ef63b4c
Maag <maagmoretz@gmail.com>
2024-01-16 16:08:45 -0300

    Update class-index-page.php

--- admin/pages/class-index-page.php
+++ admin/pages/class-index-page.php
@@ -4,9 +4,9 @@ require_once plugin_dir_path( __FILE__ ) . 'class-pages.php';
 
 class CH_Admin_Page_Default extends CH_Admin_Page {
 
-    protected $title = 'Cards in homepage';
-    protected $slug =  'cards_homepage';
-    protected $icon = 'dashicons-tagcloud';
+    protected $title = pluginTitle;
+    protected $slug =  pluginSlug;
+    protected $icon = pluginIcon;
     protected $position = 2;
 
     public function render() {
