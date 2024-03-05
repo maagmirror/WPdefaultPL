cf2bd60b57edc0f6027540e1a21365f20dc900cf
Maag <maagmoretz@gmail.com>
2024-03-05 09:41:45 -0300

    Update styles.php

--- styles.php
+++ styles.php
@@ -1,8 +1,11 @@
 <?php
 
-add_action( 'wp_enqueue_scripts',  function() {
+add_action( 'wp_enqueue_scripts', function() {
+    $style_path = '/css/styles.css'; // Ruta relativa a la hoja de estilos dentro del plugin
     wp_enqueue_style(
-        'ch-styles', 
-        plugins_url( '/css/styles.css', __DIR__ . '..' )
+        pluginSlug, 
+        plugins_url( $style_path, __FILE__ ), // Usar __FILE__ para obtener la URL del archivo correctamente
+        array(), // Dependencias, si las hay
+        filemtime( plugin_dir_path( __FILE__ ) . $style_path ) // Versión basada en la última modificación del archivo
     );
 } );
