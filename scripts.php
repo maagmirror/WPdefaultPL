b20e79bc4bb2cf157e97db802bbbde122fb49f66
Maag <maagmoretz@gmail.com>
2024-03-05 09:41:54 -0300

    Update scripts.php

--- scripts.php
+++ scripts.php
@@ -1,8 +1,11 @@
 <?php
 
 add_action( 'wp_enqueue_scripts', function() {
+    $script_path = '/js/script.js'; // Ruta relativa al archivo dentro del plugin
     wp_enqueue_script(
-		'ch-verify',
-		plugins_url( '/js/script.js', __DIR__ . '..' )
-	);
+        pluginSlug,
+        plugins_url( $script_path, __FILE__ ), // __FILE__ apunta al archivo actual
+        array(), // Dependencias, si las hay, como array('jquery')
+        filemtime( plugin_dir_path( __FILE__ ) . $script_path ) // Versión basada en la última modificación del archivo
+    );
 } );
