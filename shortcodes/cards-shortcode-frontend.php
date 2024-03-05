0c62cbd52c5272c3b6ad066f171622de505c09e3
Maag <maagmoretz@gmail.com>
2024-03-05 09:42:30 -0300

    Update cards-shortcode-frontend.php

--- shortcodes/cards-shortcode-frontend.php
+++ shortcodes/cards-shortcode-frontend.php
@@ -1,14 +1,14 @@
 <?php
 
 add_shortcode( 
-    'ch_cards', 
+    'default_shortcode', 
     function( $atts ) {
 
         ob_start();
         #esto es si queres traer lo que se va a imprimir de otro lado, el shortcode lo usas con [ch_cards]
         //require plugin_dir_path( __FILE__ ) . '/../template-parts/frontend/cardsasd.php';
 
-        echo 'esto funka';
+        echo 'helloww wooorld';
         $content = ob_get_contents();
         ob_end_clean();
 
